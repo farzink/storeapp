d236e822ab1cb03e1ff754140c0c1515f8f899d1
f-project <farzin_fz@yahoo.com>
2017-03-26 08:40:19 +0200

    3/26/2017 - 8:40 - Germany

---
+++ src/feature/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    selector: 'register-component',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
+})
+export class HomeComponent {
+    constructor(){
+    }
+}
