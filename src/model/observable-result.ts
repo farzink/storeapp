926d1b818a9172e6f4dac39940318759f20aaceb
farzink <farzin_fz@yahoo.com>
2017-09-10 20:20:16 +0200

    no message

--- src/model/observable-result.ts
+++
@@ -1,6 +0,0 @@
-import { WebCallResult } from '../utility/webcall-result';
-export class ObservableResult{
-    success(){}
-    error(){}
-    complete(){}
-}
--- src/model/register.model.ts
+++
@@ -1,7 +0,0 @@
-export class RegisterModel{
-    constructor(
-        public rememberMe:Boolean,
-        public email: string,
-        public password: string
-    ){}
-}
